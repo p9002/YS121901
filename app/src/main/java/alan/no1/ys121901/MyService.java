e82dbebc3f12973af6d286f8c125354359bef5d5
alan <p9002>
2016-12-19 10:28:55 +0800

    在Notification推播內容頁面增加Service按鍵

--- app/src/main/java/alan/no1/ys121901/MyService.java
+++ app/src/main/java/alan/no1/ys121901/MyService.java
@@ -3,9 +3,22 @@ package alan.no1.ys121901;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class MyService extends Service {
     public MyService() {
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d("SERV", "This is onCreate");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("SERV", "This is onStartCommand");
+        return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
