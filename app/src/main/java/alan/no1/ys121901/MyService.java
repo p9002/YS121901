967b860001b3766e1eb79e9d536f5b0d85de338d
alan <p9002>
2016-12-19 11:29:17 +0800

    當Notification推播內容頁面之Call Service按鍵被按下進行背景執行功能時 增加Notification推播通知_執行中

--- app/src/main/java/alan/no1/ys121901/MyService.java
+++ app/src/main/java/alan/no1/ys121901/MyService.java
@@ -1,6 +1,9 @@
 package alan.no1.ys121901;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.IBinder;
@@ -9,7 +12,7 @@ import android.util.Log;
 import java.util.Date;
 
 public class MyService extends Service {
-
+    NotificationManager nm;
     Handler handler = new Handler();
     Runnable showTime = new Runnable() {
         @Override
@@ -33,6 +36,7 @@ public class MyService extends Service {
         Log.d("SERV", "This is onStartCommand");
 
         handler.post(showTime);
+        showNotification();
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -46,5 +50,22 @@ public class MyService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private void showNotification()
+    {
+        Context context = getApplicationContext();
+        nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification.Builder builder = new Notification.Builder(context);
+        builder.setSmallIcon(R.mipmap.ic_launcher)
+                .setContentText("點擊此處回到主程式")
+                .setContentTitle("執行中");
+        Notification notification = builder.build();
+        notification.flags |= Notification.FLAG_NO_CLEAR;
+        notification.flags |= Notification.FLAG_ONGOING_EVENT;
+        nm.notify(123, notification);
+
+
+
     }
 }
