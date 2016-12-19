11f5e8e648bdcd43e7c700ee840ad3c20274774e
alan <p9002>
2016-12-19 11:50:50 +0800

    當點擊Notification推播通知_執行中,使回到Notification推播內容頁面 在點擊Stop Service按鍵時,使Notification推播通知_執行中消失

--- app/src/main/java/alan/no1/ys121901/MyService.java
+++ app/src/main/java/alan/no1/ys121901/MyService.java
@@ -2,6 +2,7 @@ package alan.no1.ys121901;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -12,6 +13,7 @@ import android.util.Log;
 import java.util.Date;
 
 public class MyService extends Service {
+    final int NOTIFICATON_REQUEST_CODE = 123;
     NotificationManager nm;
     Handler handler = new Handler();
     Runnable showTime = new Runnable() {
@@ -44,6 +46,7 @@ public class MyService extends Service {
     public void onDestroy() {
         super.onDestroy();
         handler.removeCallbacks(showTime);
+        nm.cancel(NOTIFICATON_REQUEST_CODE);
     }
 
     @Override
@@ -55,17 +58,19 @@ public class MyService extends Service {
     private void showNotification()
     {
         Context context = getApplicationContext();
+        Intent it = new Intent(context, MainActivity.class);
+        PendingIntent pi = PendingIntent
+                .getActivity(context, 1, it, PendingIntent.FLAG_UPDATE_CURRENT);
+
         nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         Notification.Builder builder = new Notification.Builder(context);
         builder.setSmallIcon(R.mipmap.ic_launcher)
                 .setContentText("點擊此處回到主程式")
-                .setContentTitle("執行中");
+                .setContentTitle("執行中")
+                .setContentIntent(pi);
         Notification notification = builder.build();
         notification.flags |= Notification.FLAG_NO_CLEAR;
         notification.flags |= Notification.FLAG_ONGOING_EVENT;
-        nm.notify(123, notification);
-
-
-
+        nm.notify(NOTIFICATON_REQUEST_CODE, notification);
     }
 }
