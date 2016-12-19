6120f32db2fa93fe53893542ea254690e26e8fc1
alan <p9002>
2016-12-19 14:33:07 +0800

    增加Regist Screen On/Off按鍵

---
+++ app/src/main/java/alan/no1/ys121901/MyReceiver.java
@@ -0,0 +1,16 @@
+package alan.no1.ys121901;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class MyReceiver extends BroadcastReceiver {
+    public MyReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("SCREEN", "Screen off...");
+    }
+}
